63465a689106769d23460a8d3abdca938ab72947
fdl <mfhisyam5@gmail.com>
2022-06-12 01:40:49 +0800

    commit 3 : 12 jun 01.40

--- app/src/main/java/com/c22pc415/felicare/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/c22pc415/felicare/ui/register/RegisterActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
-import android.widget.Toast
+import android.widget.*
 import com.c22pc415.felicare.MainActivity
 import com.c22pc415.felicare.R
 import com.c22pc415.felicare.databinding.ActivityRegisterBinding
@@ -14,16 +14,23 @@ import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
-    private lateinit var binding: ActivityRegisterBinding
+    //private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityRegisterBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        setContentView(R.layout.activity_register)
         super.onCreate(savedInstanceState)
 
-        binding.btnRegister.setOnClickListener {
-            val email = binding.etEmailRegister.text.toString().trim()
-            val pass = binding.etPasswordRegister.text.toString().trim()
-            val confirmPass = binding.etNameCat.text.toString().trim()
+        val btnRegister : Button = findViewById(R.id.btn_register)
+        val etEmailRegister : EditText = findViewById(R.id.et_email_register)
+        val etPasswordRegister : EditText = findViewById(R.id.et_password_register)
+        val etPasswordReenter : EditText = findViewById(R.id.et_password_reenter)
+        val btnBack : ImageButton = findViewById(R.id.btn_backtologin)
+
+        btnRegister.setOnClickListener {
+            val email = etEmailRegister.text.toString().trim()
+            val pass = etPasswordRegister.text.toString().trim()
+            val confirmPass = etPasswordReenter.text.toString().trim()
 
             CustomDialog.showLoading(this)
             if (checkValidation(email, pass, confirmPass)){
@@ -31,7 +38,7 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
 
-        binding.btnBack.setOnClickListener {
+        btnBack.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
@@ -53,27 +60,30 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun checkValidation(email: String, pass: String, confirmPass: String): Boolean {
+        val etEmailRegister : EditText = findViewById(R.id.et_email_register)
+        val etPasswordRegister : EditText = findViewById(R.id.et_password_register)
+        val etPasswordReenter : EditText = findViewById(R.id.et_password_reenter)
         if (email.isEmpty()){
-            binding.etEmailRegister.error = "Please field your email"
-            binding.etEmailRegister.requestFocus()
+            etEmailRegister.error = "Please field your email"
+            etEmailRegister.requestFocus()
         }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            binding.etEmailRegister.error = "Please use valida email"
-            binding.etEmailRegister.requestFocus()
+            etEmailRegister.error = "Please use valida email"
+            etEmailRegister.requestFocus()
         }else if (pass.isEmpty()){
-            binding.etPasswordRegister.error = "Please field your password"
-            binding.etPasswordRegister.requestFocus()
+            etPasswordRegister.error = "Please field your password"
+            etPasswordRegister.requestFocus()
         }else if (confirmPass.isEmpty()){
-            binding.etNameCat.error = "Please field your confirm password"
-            binding.etNameCat.requestFocus()
+            etPasswordReenter.error = "Please field your confirm password"
+            etPasswordReenter.requestFocus()
         }else if (pass != confirmPass){
-            binding.etPasswordRegister.error = "Your pass didnt match"
-            binding.etNameCat.error = "Your confirm pass didnt match"
+            etPasswordRegister.error = "Your pass didnt match"
+            etPasswordReenter.error = "Your confirm pass didnt match"
 
-            binding.etPasswordRegister.requestFocus()
-            binding.etNameCat.requestFocus()
+            etPasswordRegister.requestFocus()
+            etPasswordReenter.requestFocus()
         }else{
-            binding.etPasswordRegister.error = null
-            binding.etNameCat.error = null
+            etPasswordRegister.error = null
+            etPasswordReenter.error = null
             return true
         }
         CustomDialog.hideLoading()
